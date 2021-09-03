5273fd02abc3d7e862e205e51b680288fdf5deeb
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-09-03 18:41:11 +0100

    Worked on header

--- src/Mobile.js
+++ src/Mobile.js
@@ -8,14 +8,14 @@ export default function MobileLayout() {
       <div className="Header">
         <div className="title_section">
           <div className="search">
-            <box-icon name="search"></box-icon>
+            <box-icon name="search" size="sm"></box-icon>
           </div>
           <div className="shop_name">
             <p>Make Shopping</p>
             <p className="bold">SIMPLE</p>
           </div>
           <div className="cart">
-            <box-icon name="cart-alt"></box-icon>
+            <box-icon name="cart-alt" size="sm"></box-icon>
           </div>
         </div>
       </div>
