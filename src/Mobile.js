9c2d89273d82f798db626fde9c5be155a46c31bc
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-09-04 14:55:24 +0100

    Added Icons to head menu

--- src/Mobile.js
+++ src/Mobile.js
@@ -11,11 +11,43 @@ export default function MobileLayout() {
             <box-icon name="search" size="sm"></box-icon>
           </div>
           <div className="shop_name">
-            <p>Make Shopping</p>
-            <p className="bold">SIMPLE</p>
+            <p>Make home</p>
+            <p className="bold">BEAUTIFUL</p>
           </div>
           <div className="cart">
             <box-icon name="cart-alt" size="sm"></box-icon>
+          </div>
+        </div>
+        <div className="head_menu">
+          <div className="logo_holder">
+            <img
+              src="https://img.icons8.com/ios/50/000000/star.png"
+              className="i8_size"
+            />
+          </div>
+          <div className="logo_holder">
+            <img
+              src="https://img.icons8.com/ios/50/000000/chair.png"
+              className="i8_size"
+            />
+          </div>
+          <div className="logo_holder">
+            <img
+              src="https://img.icons8.com/ios/50/000000/three-seater-sofa.png"
+              className="i8_size"
+            />
+          </div>
+          <div className="logo_holder">
+            <img
+              src="https://img.icons8.com/ios/50/000000/table.png"
+              className="i8_size"
+            />
+          </div>
+          <div className="logo_holder">
+            <img
+              src="https://img.icons8.com/ios/50/000000/bed.png"
+              className="i8_size"
+            />
           </div>
         </div>
       </div>
